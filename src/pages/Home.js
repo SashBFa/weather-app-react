26a6c9106b759f52933d0c1c092a5695ec59a6ae
SashBFa <bastienfajs@gmail.com>
2022-04-14 12:19:07 +0200

    show nav with search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import Search from "../components/Search";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const search = useSelector((state) => state.searchReducer);
+  const [empty, setEmpty] = useState("");
+  useEffect(() => {
+    setEmpty(search);
+  }, []);
+  console.log(search);
   return (
     <div className="home">
-      <Navigation />
+      {empty && <Navigation />}
       <Search />
     </div>
   );
